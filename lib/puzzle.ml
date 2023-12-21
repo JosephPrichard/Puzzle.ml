930d1c29acdb31bec1313216112e7f9b5ddaf081
JosephPrichard <58538077+JosephPrichard@users.noreply.github.com>
2023-12-21 13:39:13 -0600

    Fixed naming issue

--- lib/puzzle.ml
+++ lib/puzzle.ml
@@ -144,10 +144,10 @@ let tiles_of_chan ic =
   let str = str_of_ch ic in
   tiles_of_str str
 
-let rec tiles_of_number tiles i = 
+let rec int_of_tiles tiles i = 
   let exp = int_of_float (10. ** float_of_int i) in
   if i < Array.length tiles then
-    int_of_tile tiles.(i) * exp + tiles_of_number tiles (i+1)
+    int_of_tile tiles.(i) * exp + int_of_tiles tiles (i+1)
   else 
     0
 
@@ -226,7 +226,7 @@ let rec add_neighbors puzzles frontier visited =
   (* Try to add the neighbor to frontier but only if it is not visited *)
   match puzzles with
   | puzzle :: puzzles ->
-    let key = (tiles_of_number puzzle.tiles 0) in
+    let key = (int_of_tiles puzzle.tiles 0) in
     (* A neighbor can only be added into the frontier if it has not already been visited*)
     (match VisitedSet.find_opt key visited with
     | Some _ ->
@@ -242,7 +242,7 @@ let rec search frontier visited bound =
   | Some (key, puzzle) ->
     let frontier = OrderedPuzzles.remove key frontier in
     (* A puzzle must be removed from the frontier and added to visited set - we don't want to search it again *)
-    let visited = VisitedSet.add (tiles_of_number puzzle.tiles 0) () visited in
+    let visited = VisitedSet.add (int_of_tiles puzzle.tiles 0) () visited in
     (* Check if the puzzle matches the goal solution *)
     if puzzle.tiles = goal_tiles then
       reconstruct_path puzzle []
@@ -254,7 +254,7 @@ let rec search frontier visited bound =
     
 let solve tiles =
   let root = {parent = None; tiles = tiles; gscore = 0; fscore = 0; move = None} in
-  let root_key = tiles_of_number root.tiles 0 in
+  let root_key = int_of_tiles root.tiles 0 in
   let frontier = OrderedPuzzles.add root_key root OrderedPuzzles.empty in
   let visited = VisitedSet.empty in
   search frontier visited max_int
